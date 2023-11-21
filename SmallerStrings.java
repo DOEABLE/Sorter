37900f75da74a90dc0d4c14ae72493708e44496d
DOEABLE <111475042+DOEABLE@users.noreply.github.com>
2023-11-21 16:26:46 +0900

    p보다 크기가 작은 부분문자열의 갯수 구하기
    예를 들어, t="3141592"이고 p="271" 인 경우, t의 길이가 3인 부분 문자열은 314, 141, 415, 159, 592. 이 문자열이 나타내는 수 중 p인 271보다 작거나 같은 수는 141, 159 2개

--- SmallerStrings.java
+++ SmallerStrings.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
 
 public class SmallerStrings {
-    public static int solution(String t, String p){
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("문자열 t를 입력하세요(t의 자릿 수는 1~18까지 입니다.): ");
+        String t = String.valueOf(scanner.nextInt());
+        String p = String.valueOf(scanner.nextInt());
+        int result = solution(t,p);
+        System.out.println(result);
+    }
 
+    public static int solution(String t, String p){
+        Scanner scanner = new Scanner(System.in);
         //숫자로 이루어진 문자열 t와 p가 주어질 때, t에서 p와 길이가 같은 부분문자열 중에서, 이 부분문자열이 나타내는 수가 p가 나타내는 수보다 작거나 같은 것이 나오는 횟수를 return하는 함수 solution
         int answer = 0;
         Long pInt = Long.parseLong(p);  //p문자열은 최대 18이므로 long type 적용
@@ -16,14 +23,6 @@ public class SmallerStrings {
         }
         return answer;
     }
-
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        String t = String.valueOf(scanner.nextInt());
-        String p = String.valueOf(scanner.nextInt());
-        int result = solution(t,p);
-    }
-
 }
 
 
